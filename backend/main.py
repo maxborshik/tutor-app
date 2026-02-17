17c4209094bc7f022108592e3852c167fcd08dd2
maxborshik <maxborshik@icloud.com>
2026-02-17 18:08:36 +0000

    started SQLModel classes, worked on postgres

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from datetime import datetime
+from typing import Optional
+import os
 
+# connect to db
+postgres_url = os.environ["DATABASE_URL"]
+
+# build app
 app = FastAPI()
 
 
+# health checks
 @app.get("/")
 def read_root():
     return {"status": "Backend is Online", "framework": "FastAPI"}
@@ -11,3 +20,18 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+# sqlmodel class setups
+
+
+class student(SQLModel, table=True):
+    def __init__(self):
+        id: Optional[int] = Field(default=None, primary_key=True)
+        first_name: str
+        last_name: str
+        email: str = Field(unique=True, index=True)
+        date_added: datetime
+        age: Optional[int] = None
+        bio: Optional[str] = None
+        subjects: Optional[list]
